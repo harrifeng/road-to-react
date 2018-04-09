010aa70b232540f661a0fa9c5aab46f75e8b8f65
Haoran Feng <harrifeng@gmail.com>
2018-04-09 20:18:26 +0800

    easy to work

--- hackernew/src/App.js
+++ hackernew/src/App.js
@@ -31,15 +31,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
+        {this.state.list.map(
+          item => {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+            )
+          }
         )}
       </div>
     );
