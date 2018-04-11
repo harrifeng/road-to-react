f94f1ec1e97a0b1af8c5015e0fa6bf9054ceef22
Haoran Feng <harrifeng@gmail.com>
2018-04-11 20:28:16 +0800

    alignment

--- hackernew/src/App.js
+++ hackernew/src/App.js
@@ -89,4 +89,50 @@ class App extends Component {
   }
 }
 
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+
+    return (
+      <div>
+        {
+          list.filter(isSearched(pattern)).map(
+            item =>
+              <div key={item.objectID}>
+                  <span>
+                      <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                      <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                          <span>
+                              <button
+                                  onClick={()=> onDismiss(item.objectID)}
+                                  type="button"
+                                           >
+                                </button>
+                            </span>
+                </div>
+          )
+      }
+      </div>
+    );
+  }
+}
+
 export default App;
