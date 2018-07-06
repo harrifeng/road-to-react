cc6960d5c41e15caaee2bcdd5dbcfc68c838fc53
Haoran Feng <harrifeng@gmail.com>
2018-07-06 14:36:02 +0800

    go on ok

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://github.com/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/redux',
+    author: 'Dan Abramov',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          list.map(function(item) {
+            return (
+              <div key={item.objectID}>
+                {item.title}
+              </div>)
+          })
+        }
       </div>
     );
   }
--- hackernews/src/index.js
+++ hackernews/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <h1>Hello React World</h1>,
+  <App/>,
   document.getElementById('root'));
