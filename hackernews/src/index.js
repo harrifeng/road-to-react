35042cdf5de6b09972c84712247b9da8502f8121
Haoran Feng <harrifeng@gmail.com>
2018-07-06 14:19:46 +0800

    page 31 works

--- hackernews/src/index.js
+++ hackernews/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <h1>Hello React World</h1>,
+  document.getElementById('root'));
