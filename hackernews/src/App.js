25011243284fd3acf794c8aca084270c04e59835
Haoran Feng <harrifeng@gmail.com>
2018-07-06 15:12:27 +0800

    format

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -33,16 +33,18 @@ class App extends Component {
     return (
       <div className="App">
         {
-          this.state.list.map(item =>
-                   <div key={item.objectID}>
-                       <span>
-                           <a href={item.url}>{item.title}</a>
-                         </span>
-                         <span>{item.author}</span>
-                           <span>{item.num_comments}</span>
-                             <span>{item.points}</span>
-                     </div>
-                  )
+          this.state.list.map(item => {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+            );
+          })
         }
       </div>
     );
