57c49425b21ede616b0dd44356b16f0d8973877a
Haoran Feng <harrifeng@gmail.com>
2018-07-06 14:43:19 +0800

    arrow on 25 again

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -26,17 +26,16 @@ class App extends Component {
     return (
       <div className="App">
         {
-          list.map(item => {
-            return (
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </div>)
-          })
+          list.map(item =>
+                   <div key={item.objectID}>
+                       <span>
+                           <a href={item.url}>{item.title}</a>
+                         </span>
+                         <span>{item.author}</span>
+                           <span>{item.num_comments}</span>
+                             <span>{item.points}</span>
+                     </div>
+                  )
         }
       </div>
     );
